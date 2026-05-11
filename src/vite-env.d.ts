f642750f27880096cca1661c29e5d8a6b01724a9
ryan.hillhouse <ryan.hillhouse@students.fhnw.ch>
2026-05-11 06:46:17 +0000

    feat: add type declarations for cytoscape extensions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,29 @@
 /// <reference types="vite/client" />
 
+declare module 'cytoscape-elk' {
+  import cytoscape from 'cytoscape';
+  const elk: cytoscape.Ext;
+  export default elk;
+}
+
+declare module 'cytoscape-cola' {
+  import cytoscape from 'cytoscape';
+  const cola: cytoscape.Ext;
+  export default cola;
+}
+
+declare module 'cytoscape-dagre' {
+  import cytoscape from 'cytoscape';
+  const dagre: cytoscape.Ext;
+  export default dagre;
+}
+
+declare module 'cytoscape-d3-force' {
+  import cytoscape from 'cytoscape';
+  const d3Force: cytoscape.Ext;
+  export default d3Force;
+}
+
 interface ImportMetaEnv {
   readonly VITE_BASE_URL: string;
   readonly VITE_USERNAME: string;
